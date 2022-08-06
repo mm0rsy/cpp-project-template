40278f432194f935d62e2d8eb27570b182059a6d
Mohamed-Abdulaty <mohamed.e.abdulaty@gmail.com>
2022-08-06 23:50:34 +0200

    F1: Sample helloWorld program.

---
+++ application/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * *        Author: Mohamed Morsy
+ * *   Create Time: 2022-08-06 22:02:33
+ * *   Modified by: Mohamed Morsy
+ * * Modified time: 2022-08-06 23:48:45
+ * *     File Name: main.cpp
+ * *   Description: A main file for a sample application.
+ * *       License: MIT
+ */
+
+#include <iostream>
+
+/**
+ * \brief   main function
+ *
+ * \return int
+ */
+int main() {
+  /* code */
+  std::cout << "Hello, world!" << std::endl;
+  return 0;
+}
